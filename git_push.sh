f762dbd49b4fbc9b8f326c8c4e7b84077278d069
karaage0703 <coldchicken@gmail.com>
2022-12-02 11:47:11 +0900

    Update zoo number

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-git add ./zoo_001/images/info.csv
-git add ./zoo_001/images/*.png
+git add ./zoo_002_OMMF2022/images/info.csv
+git add ./zoo_002_OMMF2022/images/*.png
 git commit -m "Add animal"
 git push
